e635a5e5387353b84452a674328b58cab543d22d
nilsJavaNoob <nils1968@gmail.com>
2018-09-29 09:16:04 +0300

    noteb

--- pr1.asm
+++ pr1.asm
@@ -1,3 +1,8 @@
-.include "m8def.inc"
-
-		ldi r16, 0XFF
+//git pull https://github.com/nilsJavaNoob/asm.git
+.include "m8Adef.inc"
+	
+metka:	ldi R16,0xff	//loadied 255 to r16(RON)
+		out DDRC,R16	// непосредственная команда вывода числа из РОН в РВВ
+						//сделали все биты порта В выводами
+		out PORTC,R16	//установили на всех выходах PORTB логическую единицу 
+		rjmp metka
